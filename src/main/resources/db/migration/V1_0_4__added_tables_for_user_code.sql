72440336b9d6eb073cbd952992f45fbefbe8e969
Marcin Holota <marcinholota1@gmail.com>
2018-05-02 17:46:47 +0200

    [US0014] - Tables for users codes have been added

--- src/main/resources/db/migration/V1_0_4__added_tables_for_user_code.sql
+++ src/main/resources/db/migration/V1_0_4__added_tables_for_user_code.sql
@@ -1,61 +1,72 @@
-CREATE SEQUENCE programming_languages_seq;
-CREATE TABLE programming_languages
+CREATE SEQUENCE programming_language_seq;
+
+CREATE TABLE programming_language
 (
-  id INTEGER DEFAULT nextval('programming_languages_seq') PRIMARY KEY,
+  id INTEGER DEFAULT nextval('programming_language_seq') PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE SEQUENCE categories_seq;
-CREATE TABLE categories
+
+CREATE SEQUENCE category_seq;
+
+CREATE TABLE category
 (
-  id INTEGER DEFAULT nextval('categories_seq') PRIMARY KEY,
+  id INTEGER DEFAULT nextval('category_seq') PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE SEQUENCE problems_seq;
-CREATE TABLE problems
+
+CREATE SEQUENCE problem_seq;
+
+CREATE TABLE problem
 (
-  id INTEGER NOT NULL DEFAULT nextval('problems_seq') PRIMARY KEY,
+  id INTEGER NOT NULL DEFAULT nextval('problem_seq') PRIMARY KEY,
   name VARCHAR (50) NOT NULL,
   author VARCHAR(50) REFERENCES users(username),
-  category INTEGER REFERENCES categories(id),
+  category INTEGER REFERENCES category(id),
   content VARCHAR NOT NULL
 );
 
-CREATE TABLE available_programming_languages
+
+CREATE TABLE available_programming_language
 (
-  problem_id INTEGER REFERENCES problems(id),
-  programming_language_id INTEGER REFERENCES  programming_languages(id),
+  problem_id INTEGER REFERENCES problem(id),
+  programming_language_id INTEGER REFERENCES  programming_language(id),
   PRIMARY KEY (problem_id, programming_language_id)
 );
 
-CREATE SEQUENCE tests_seq;
-CREATE TABLE tests
+
+CREATE SEQUENCE test_seq;
+
+CREATE TABLE test
 (
-  id INTEGER DEFAULT nextval('tests_seq') PRIMARY KEY,
-  problem_id INTEGER REFERENCES problems(id),
+  id INTEGER DEFAULT nextval('test_seq') PRIMARY KEY,
+  problem_id INTEGER REFERENCES problem(id),
   number INTEGER NOT NULL,
   is_open BOOLEAN NOT NULL DEFAULT FALSE,
   input VARCHAR NOT NULL,
   output VARCHAR NOT NULL
 );
 
-CREATE SEQUENCE submits_seq;
-CREATE TABLE submits
+
+CREATE SEQUENCE submit_seq;
+
+CREATE TABLE submit
 (
-  id INTEGER NOT NULL DEFAULT nextval('submits_seq') PRIMARY KEY,
+  id INTEGER NOT NULL DEFAULT nextval('submit_seq') PRIMARY KEY,
   author VARCHAR(50) REFERENCES users(username),
-  problem_id INTEGER REFERENCES problems(id),
+  problem_id INTEGER REFERENCES problem(id),
   received_time TIMESTAMP WITHOUT TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
   pending BOOLEAN DEFAULT FALSE,
   compiler_output VARCHAR,
-  programming_language_id INTEGER REFERENCES programming_languages(id)
+  programming_language_id INTEGER REFERENCES programming_language(id)
 );
 
-CREATE TABLE submits_results
+
+CREATE TABLE submit_result
 (
-  submit_id INTEGER REFERENCES submits(id),
-  test_id INTEGER REFERENCES tests(id),
+  submit_id INTEGER REFERENCES submit(id),
+  test_id INTEGER REFERENCES test(id),
   output VARCHAR NOT NULL,
   is_correct BOOLEAN NOT NULL,
   PRIMARY KEY (submit_id, test_id)
