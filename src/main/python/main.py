03acf1fe25535d96e44f4ff0570e5638ccd54598
addme <addme96@gmail.com>
2018-05-08 23:01:13 +0200

    Added python submit processor configuration table. Changes in python files.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,10 @@
 from submit_processor import SubmitProcessor
 import sys
 
+
 def main():
-    submit_processor = SubmitProcessor()
+    working_directory =  sys.argv[3]
+    submit_processor = SubmitProcessor(working_directory)
     submit_id = sys.argv[1]
     problem_id = sys.argv[2]
     submit_processor.process_submit(submit_id, problem_id)
