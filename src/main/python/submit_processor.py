3ed5ed80cc5a53e16c113244fe46357df8f692d3
addme <addme96@gmail.com>
2018-05-08 23:01:13 +0200

    Added python submit processor configuration table. Changes in python files.

--- src/main/python/submit_processor.py
+++ src/main/python/submit_processor.py
@@ -1,13 +1,26 @@
 import db_connection
 import subprocess
 import os
+from time import sleep
 
 
 class SubmitProcessor:
+    def __init__(self, working_directory):
+        """
+        :param working_directory: IMPORTANT: User running SubmitProcessor must have read/write permissions to working_directory
+        """
+        self.working_directory = working_directory
 
     def process_submit(self, submit_id, problem_id):
-        self.wait_for_submit_transaction_commit(submit_id)
-        directory = '/home/addme/IdeaProjects/justlearnit_us0005/src/main/python/' + str(submit_id)
+        """
+        :param submit_id: integer field from database
+        :param problem_id: integer field from database
+        :return: None
+        """
+        is_transaction_commited = self.wait_for_submit_transaction_commit(submit_id)
+        if not is_transaction_commited:
+            return
+        directory = self.working_directory + str(submit_id)
         if os.path.exists(directory):
             self.remove_directory_with_source(directory)
         os.makedirs(directory)
@@ -23,21 +36,35 @@ class SubmitProcessor:
         self.set_submit_as_processed_in_database(submit_id)
 
     def wait_for_submit_transaction_commit(self, submit_id):
+        """
+        :param submit_id: integer field from database
+        :return: True if transaction committed, False if not
+        """
         conn = db_connection.get_connection()
         conn.autocommit = True
         cur = conn.cursor()
         query = 'SELECT id FROM submit WHERE processed IS NOT TRUE AND id = {0:s};'.format(str(submit_id))
         transaction_committed = False
-        endless_loop_protection = 10000
+        endless_loop_protection = 100
+        result = []
         while not transaction_committed and endless_loop_protection > 0:
             cur.execute(query)
             result = cur.fetchall()
             if result is True:
                 transaction_committed = True
             endless_loop_protection -= 1
+            sleep(0.1)
         conn.close()
+        if endless_loop_protection == 0 and not result:
+            return False
+        return True
 
     def create_a_file(self, directory, unprocessed_submit_id):
+        """
+        :param directory: Directory to process particular submit in.
+        :param unprocessed_submit_id: integer field from database
+        :return: None
+        """
         file = open(directory + '/Main.java', mode='w+', encoding='utf-8')
         conn = db_connection.get_connection()
         cur = conn.cursor()
@@ -48,6 +75,11 @@ class SubmitProcessor:
         file.write(code_content)
 
     def compile_java(self, directory):
+        """
+        javac must be in PATH environment variable
+        :param directory: Directory to process particular submit in.
+        :return: Tuple (return_code, stdout, stderr)
+        """
         process = subprocess.Popen(['javac', directory + '/Main.java'], encoding='utf-8', stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
         process.wait()
@@ -57,6 +89,9 @@ class SubmitProcessor:
         return process_return_code, stdout, stderr
 
     def run_tests_java(self, directory, problem_id, submit_id):
+        """
+        java must be in PATH environment variable
+        """
         tests = self.get_tests(problem_id)
         for test in tests:
             test_id, test_input = test
--- src/main/resources/db/migration/V1_0_5__modified_submit_table.sql
+++ src/main/resources/db/migration/V1_0_5__modified_submit_table.sql
@@ -1,13 +1,14 @@
 ALTER TABLE public.problem
   DROP COLUMN category;
 
-ALTER TABLE public.category DROP COLUMN id;
+ALTER TABLE public.category
+  DROP COLUMN id;
 
 ALTER TABLE public.category
   ADD PRIMARY KEY (name);
 
 ALTER TABLE public.problem
-  ADD COLUMN category_name character varying(50) REFERENCES public.category(name);
+  ADD COLUMN category_name character varying(50) REFERENCES public.category (name);
 
 ALTER TABLE public.available_programming_language
   DROP COLUMN programming_language_id;
@@ -23,13 +24,13 @@ ALTER TABLE programming_language
   ADD PRIMARY KEY (name);
 
 ALTER TABLE public.available_programming_language
-  ADD COLUMN programming_language_name character varying(50) REFERENCES public.programming_language(name);
+  ADD COLUMN programming_language_name character varying(50) REFERENCES public.programming_language (name);
 
 ALTER TABLE public.available_programming_language
   ADD PRIMARY KEY (problem_id, programming_language_name);
 
 ALTER TABLE public.submit
-  ADD COLUMN programming_language_name character varying(50) REFERENCES public.programming_language(name);
+  ADD COLUMN programming_language_name character varying(50) REFERENCES public.programming_language (name);
 
 
 ALTER TABLE public.submit
@@ -60,55 +61,84 @@ ALTER TABLE public.submit_result
   ADD COLUMN execution_stdout character varying,
   ADD COLUMN execution_stderr character varying;
 
-CREATE OR REPLACE FUNCTION submit_result_evaluate_if_test_passed() RETURNS trigger AS $$
-  BEGIN
-    IF NEW.execution_stdout = (SELECT output FROM test t WHERE t.id = NEW.test_id)
-    THEN
-        NEW.is_correct := true;
-    ELSE
-        NEW.is_correct := false;
-    END IF;
-  RETURN NEW;
-  END
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER submit_result_insert_trigger
-BEFORE INSERT ON submit_result
-FOR EACH ROW
-EXECUTE PROCEDURE submit_result_evaluate_if_test_passed();
-
-
---CREATE PROCEDURAL LANGUAGE 'plpythonu' HANDLER plpython_call_handler;
-
-CREATE EXTENSION IF NOT EXISTS plpythonu;
-
-CREATE OR REPLACE FUNCTION run_submit_processor_py(submit_id integer, problem_id integer)
-  RETURNS void AS $$
-import subprocess
-python_path = '/home/addme/IdeaProjects/justlearnit_us0005/venv/bin/python3.6'
-submit_processor_path = '/home/addme/IdeaProjects/justlearnit_us0005/src/main/python/main.py'
-parameter1 = str(submit_id)
-parameter2 = str(problem_id)
-process = subprocess.Popen([python_path, submit_processor_path, parameter1, parameter2 ], stdin=None, stdout=None, stderr=None, close_fds=True)
-$$ LANGUAGE plpythonu;
-
-
-CREATE OR REPLACE FUNCTION run_submit_processor_function() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION submit_result_evaluate_if_test_passed()
+  RETURNS trigger AS $$
 BEGIN
-  PERFORM run_submit_processor_py(NEW.id, NEW.problem_id);
+  IF NEW.execution_stdout = (SELECT output
+                             FROM test t
+                             WHERE t.id = NEW.test_id)
+  THEN
+    NEW.is_correct := true;
+  ELSE
+    NEW.is_correct := false;
+  END IF;
   RETURN NEW;
 END
-$$ LANGUAGE 'plpgsql';
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER submit_result_insert_trigger
+  BEFORE INSERT
+  ON submit_result
+  FOR EACH ROW
+EXECUTE PROCEDURE submit_result_evaluate_if_test_passed();
+
+CREATE TABLE python_submit_processor_configuration
+(
+  id                       INTEGER                 NOT NULL DEFAULT 1 PRIMARY KEY,
+  python_path              character varying(1000) NOT NULL,
+  python_working_directory character varying(1000) NOT NULL,
+  CONSTRAINT only_one_row CHECK (id = 1)
+);
+
+INSERT INTO python_submit_processor_configuration (id, python_path, python_working_directory)
+VALUES (1, '/home/addme/IdeaProjects/justlearnit_us0005/venv/bin/python3.6',
+        '/home/addme/IdeaProjects/justlearnit_us0005/src/main/python');
+
+CREATE EXTENSION IF NOT EXISTS plpython3u;
+
+CREATE OR REPLACE FUNCTION run_submit_processor_py(submit_id                integer, problem_id integer,
+                                                   python_path              text, python_working_directory text)
+  RETURNS void AS $$
+import subprocess
+
+parameter1 = str(submit_id)
+parameter2 = str(problem_id)
+submit_processor_path = python_working_directory + '/main.py'
+process = subprocess.Popen([python_path, submit_processor_path, parameter1, parameter2, python_working_directory],
+                           stdin=None, stdout=None, stderr=None, close_fds=True)
+$$
+LANGUAGE plpython3u;
+
+
+CREATE OR REPLACE FUNCTION run_submit_processor_function()
+  RETURNS trigger AS $$
+DECLARE
+  python_path              text;
+  python_working_directory text;
+BEGIN
+  SELECT
+    python_path,
+    python_working_directory
+  INTO python_path, python_working_directory
+  FROM python_submit_processor_configuration
+  WHERE id = 1;
+  PERFORM run_submit_processor_py(NEW.id, NEW.problem_id, python_path, python_working_directory);
+  RETURN NEW;
+END
+$$
+LANGUAGE 'plpgsql';
 
 
 CREATE CONSTRAINT TRIGGER run_submit_processor_py
-  AFTER INSERT ON submit
+  AFTER INSERT
+  ON submit
   DEFERRABLE INITIALLY IMMEDIATE
   FOR EACH ROW
-  EXECUTE PROCEDURE run_submit_processor_function();
+EXECUTE PROCEDURE run_submit_processor_function();
 
 
 INSERT INTO programming_language (name)
-  VALUES ('Java'), ('C++');
+VALUES ('Java'), ('C++');
 
 
