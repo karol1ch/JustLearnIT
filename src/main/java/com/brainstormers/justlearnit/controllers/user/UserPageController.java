56b2d6ad0a86377fd5868afc1ca1efab3548fa97
Szymon <ctk7000@gmail.com>
2018-05-21 10:10:43 +0200

    - added registration form - added registration form permission for all in security config - added UserRegistrationController - moved UserPageController to user package

--- src/main/java/com/brainstormers/justlearnit/controllers/user/UserPageController.java
+++ src/main/java/com/brainstormers/justlearnit/controllers/user/UserPageController.java
@@ -1,4 +1,4 @@
-package com.brainstormers.justlearnit.controllers;
+package com.brainstormers.justlearnit.controllers.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
