6fb6d26f08026b7b95e620266cf79b94b251f017
Szymon <ctk7000@gmail.com>
2018-05-24 11:52:00 +0200

    - modified User and UserDetail - added registration confirmation permitall in security

--- src/main/java/com/brainstormers/justlearnit/security/config/SecurityConfig.java
+++ src/main/java/com/brainstormers/justlearnit/security/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/registrationConfirmation").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
